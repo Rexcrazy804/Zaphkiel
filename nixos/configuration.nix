4046b9fc34138aa4f4a3dd2f99f1a454b908b8c8
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-04-26 08:59:57 +0400

    kde5 -> kde6

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,8 +42,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  services.desktopManager.plasma6.enable = true;
   programs.partition-manager.enable = true;
 
   # Enable OpenGL

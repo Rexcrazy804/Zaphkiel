05ba52940f22f1961f28dfa0cff81c6367c4e547
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-06-01 12:54:12 +0400

    bumb stable packages from 23.11 to 24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     aagl.url = "github:ezKEa/aagl-gtk-on-nix";
     aagl.inputs.nixpkgs.follows = "nixpkgs"; # Name of nixpkgs input you want to use

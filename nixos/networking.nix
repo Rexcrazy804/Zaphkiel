a320cbb1a37bd6dfd2afb1981e25659e68fb66af
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-06-02 01:34:52 +0400

    change backend to iwd, one rollback

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,20 +1,30 @@
 {...}: {
-  networking.networkmanager.enable = true;
-  networking.hostName = "Zaphkiel"; # Define your hostname.
+  networking = {
+    hostName = "Zaphkiel"; # Define your hostname.
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      } # KDE Connect
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      } # KDE Connect
-    ];
+    networkmanager = {
+      enable = true;
+      wifi = {
+        powersave = false;
+        backend = "iwd";
+        macAddress = "random";
+      };
+    };
+
+    firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        } # KDE Connect
+      ];
+      allowedUDPPortRanges = [
+        {
+          from = 1714;
+          to = 1764;
+        } # KDE Connect
+      ];
+    };
   };
 }

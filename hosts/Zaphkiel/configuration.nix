b242ce3f2d0f1a551f69aba25d2f6ef5065be828
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-08-01 14:56:52 +0530

    made options for aagl

--- hosts/Zaphkiel/configuration.nix
+++ hosts/Zaphkiel/configuration.nix
@@ -12,5 +12,7 @@
     nvidia.enable = true;
   };
 
+  anime-games.enable = true;
+
   system.stateVersion = "23.11";
 }
--- modules/nixos/programs/aagl.nix
+++ modules/nixos/programs/aagl.nix
@@ -1,13 +1,19 @@
-{inputs, ...}: {
+{lib, inputs, config, ...}: {
   imports = [
     inputs.aagl.nixosModules.default
   ];
 
-  # anime games launcher stuff
-  nix.settings = inputs.aagl.nixConfig;
-  programs = {
-    anime-game-launcher.enable = false;
-    honkers-railway-launcher.enable = true;
-    sleepy-launcher.enable = true;
+  options = {
+    anime-games.enable = lib.mkEnableOption "Enable a certain Anime Game";
+  };
+
+  config = lib.mkIf config.anime-games.enable {
+    nix.settings = inputs.aagl.nixConfig;
+
+    programs = {
+      anime-game-launcher.enable = false;
+      honkers-railway-launcher.enable = true;
+      sleepy-launcher.enable = true;
+    };
   };
 }

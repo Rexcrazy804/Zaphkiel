48752cec5c05ec84b5f5968949c1ef31a7f406fd
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-08-01 18:00:44 +0530

    removed unused anime4k shadders

--- modules/home/programs/mpv.nix
+++ modules/home/programs/mpv.nix
@@ -33,75 +33,18 @@
           "CTRL+1" =
             "no-osd change-list glsl-shaders set \""
             + builtins.concatStringsSep ":" [
-              "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_VL.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_VL.glsl"
               "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
               "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
-            ]
-            + "\"; show-text \"Anime4K: Mode A (HQ)\"";
 
-          "CTRL+2" =
-            "no-osd change-list glsl-shaders set \""
-            + builtins.concatStringsSep ":" [
               "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_Soft_VL.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_VL.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
-            ]
-            + "\"; show-text \"Anime4K: Mode B (HQ)\"";
 
-          "CTRL+3" =
-            "no-osd change-list glsl-shaders set \""
-            + builtins.concatStringsSep ":" [
-              "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Upscale_Denoise_CNN_x2_VL.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
-            ]
-            + "\"; show-text \"Anime4K: Mode C (HQ)\"";
-
-          "CTRL+4" =
-            "no-osd change-list glsl-shaders set \""
-            + builtins.concatStringsSep ":" [
-              "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_VL.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_VL.glsl"
               "${shaderFolder}/Anime4K_Restore_CNN_M.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
+              "${shaderFolder}/Anime4K_Restore_CNN_VL.glsl"
+
               "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
+              "${shaderFolder}/Anime4K_Upscale_CNN_x2_VL.glsl"
             ]
             + "\"; show-text \"Anime4K: Mode A+A (HQ)\"";
-
-          "CTRL+5" =
-            "no-osd change-list glsl-shaders set \""
-            + builtins.concatStringsSep ":" [
-              "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_Soft_VL.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_VL.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_Soft_M.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
-            ]
-            + "\"; show-text \"Anime4K: Mode B+B (HQ)\"";
-
-          "CTRL+6" =
-            "no-osd change-list glsl-shaders set \""
-            + builtins.concatStringsSep ":" [
-              "${shaderFolder}/Anime4K_Clamp_Highlights.glsl"
-              "${shaderFolder}/Anime4K_Upscale_Denoise_CNN_x2_VL.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x2.glsl"
-              "${shaderFolder}/Anime4K_AutoDownscalePre_x4.glsl"
-              "${shaderFolder}/Anime4K_Restore_CNN_M.glsl"
-              "${shaderFolder}/Anime4K_Upscale_CNN_x2_M.glsl"
-            ]
-            + "\"; show-text \"Anime4K: Mode C+A (HQ)\"";
 
           "CTRL+0" = "no-osd change-list glsl-shaders clr \"\"; show-text \"GLSL shaders cleared\"";
         };

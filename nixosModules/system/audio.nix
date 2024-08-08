9a85d2b27ba086340df45997aa5bf23b3eae9788
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-08-08 20:31:14 +0530

    fix 100% powerusage on integrated camera

--- nixosModules/system/audio.nix
+++ nixosModules/system/audio.nix
@@ -6,5 +6,17 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # apparently some issue uses 100% power for whatever reason, will needa look into it later
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+          };
+        };
+      };
+    };
   };
 }

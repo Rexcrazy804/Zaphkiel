4bde732ba42b6a85758be404f7e40125c2e2de38
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-08-30 22:49:42 +0530

    zen version bump + remove amd initrd kernel module

--- hosts/Zaphkiel/hardware-configuration.nix
+++ hosts/Zaphkiel/hardware-configuration.nix
@@ -17,7 +17,7 @@
 
     initrd = {
       availableKernelModules = ["nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci"];
-      kernelModules = ["amdgpu"];
+      # kernelModules = ["amdgpu"];
     };
 
     # blacklisted to prefer zenpower
--- pkgs/zen-browser.nix
+++ pkgs/zen-browser.nix
@@ -59,11 +59,11 @@
 in
   stdenv.mkDerivation rec {
     pname = "zen-browser-bin";
-    version = "1.0.0-a.30";
+    version = "1.0.0-a.32";
 
     src = fetchzip {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-      hash = "sha256-FbNURIfTQMxOjEpVIIY13AAPU7M4Jg5mUklJzLuAxh8=";
+      hash = "sha256-IRheE/4EhK8I7aCEDmbhwSrvxISKU6e42ezAp+FnH/M=";
     };
 
     desktopItems = [

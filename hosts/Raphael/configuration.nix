3d75ee0769a49663122cd8def38342b4345c8092
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-10-26 14:35:29 +0530

    Sway: support screensharing

--- hosts/Raphael/configuration.nix
+++ hosts/Raphael/configuration.nix
@@ -30,9 +30,16 @@
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
       export _JAVA_AWT_WM_NONREPARENTING=1
+      # SCREEN SHARING
+      export MOZ_ENABLE_WAYLAND=1
     '';
   };
   security.polkit.enable = true;
+  xdg.portal = {
+    # enable = true; enabled by flat pack
+    wlr.enable = true;
+    xdgOpenUsePortal = true;
+  };
 
   # flatpak
   services.flatpak.enable = true;

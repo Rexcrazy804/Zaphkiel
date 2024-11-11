efcb8a2f6ebf1c1903e0c1a40818ab5c2dc569c9
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-11-11 14:50:01 +0400

    Age: rekeying + added Host keying

--- nixosModules/programs/age.nix
+++ nixosModules/programs/age.nix
@@ -12,5 +12,7 @@
     inputs.agenix.packages.${pkgs.system}.default
   ];
 
-  age.identityPaths = builtins.map (username: "/home/${username}/.ssh/id_ed25519") users;
+  age.identityPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+  ] ++ builtins.map (username: "/home/${username}/.ssh/id_ed25519") users;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,17 @@
 let
-  rexies = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICELSL45m4ptWDZwQDi2AUmCgt4n93KsmZtt69fyb0vy"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZTLQQzgCvdaAPdxUkpytDHgwd8K1N1IWtriY4tWSvn"
-  ];
+  users = {
+    rexies = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICELSL45m4ptWDZwQDi2AUmCgt4n93KsmZtt69fyb0vy"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZTLQQzgCvdaAPdxUkpytDHgwd8K1N1IWtriY4tWSvn"
+    ];
+  };
+
+  hosts = {
+    Zaphkiel = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEL+RcwQjikBAOFxI3GSlvB7S0E0groj2I6h4XajvMAy"
+    ];
+  };
 in {
-  "secret1.age".publicKeys = rexies;
-  "secret2.age".publicKeys = rexies;
+  "secret1.age".publicKeys = users.rexies;
+  "secret2.age".publicKeys = hosts.Zaphkiel;
 }

13438b7d25744bdf52d0d0e2da1a38f3dd9ea3f8
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-02-13 18:28:59 +0530

    Aphrodite: direnv + use neovim

--- hosts/Aphrodite/configuration.nix
+++ hosts/Aphrodite/configuration.nix
@@ -58,7 +58,27 @@
   };
 
   environment.systemPackages = builtins.attrValues {
-    inherit (pkgs) git ripgrep fd;
+    inherit (pkgs) git;
+    nixvim = pkgs.wrappedPkgs.nvim-lsp;
+  };
+
+  programs.direnv = {
+    enable = true;
+    silent = true;
+    loadInNixShell = true;
+    nix-direnv.enable = true;
+    direnvrcExtra = ''
+      : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              hash="$(sha1sum - <<< "$PWD" | head -c40)"
+              path="''${PWD//[^a-zA-Z0-9]/-}"
+              echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+          )}"
+      }
+    '';
   };
 
   system.stateVersion = "23.11";

f633d700686afb5110e32571953018ac91efd01f
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-02-18 18:05:07 +0530

    Mpv: refactor + cleanup

--- users/Wrappers/mpv/default.nix
+++ users/Wrappers/mpv/default.nix
@@ -3,7 +3,7 @@
   lib,
   anime ? false,
 }: let
-  mpvconf = let
+  mainConf = let
     conf = pkgs.writeText "mpv.conf" ''
       geometry=75%x75%
       gpu-context=wayland
@@ -12,18 +12,24 @@
       screenshot-template='%F - [%P] (%#01n)'
       vo=gpu
     '';
+  in [
+    {
+      name = "mpv.conf";
+      path = conf;
+    }
+  ];
+
+  shadderConfig = let
     shadder = pkgs.writeText "input.conf" (import ./bindings.nix);
-  in
-    pkgs.linkFarm "mods" ([
-        {
-          name = "mpv.conf";
-          path = conf;
-        }
-      ]
-      ++ (lib.optionals anime {
-        name = "input.conf";
-        path = shadder;
-      }));
+  in [
+    {
+      name = "input.conf";
+      path = shadder;
+    }
+  ];
+
+  finalConfigList = mainConf ++ (lib.optionals anime shadderConfig);
+  mpvconf = pkgs.linkFarm "mpvConfDir" finalConfigList;
 in
   pkgs.symlinkJoin {
     name = "mpv";

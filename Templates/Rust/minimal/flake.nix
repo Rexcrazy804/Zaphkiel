29d3ab9472abc406429d52320824962bbb8201d1
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-02-26 10:52:52 +0530

    Templates: rust-minimal: refactor

--- Templates/Rust/minimal/flake.nix
+++ Templates/Rust/minimal/flake.nix
@@ -24,18 +24,16 @@
       );
   in {
     packages = forAllSystems (pkgs: let
-      rust-package = let
-        toml = pkgs.lib.importTOML ./Cargo.toml;
-      in
-        pkgs.rustPlatform.buildRustPackage {
-          pname = toml.package.name;
-          version = toml.package.version;
+      toml = pkgs.lib.importTOML ./Cargo.toml;
+      rust-package = pkgs.rustPlatform.buildRustPackage {
+        pname = toml.package.name;
+        version = toml.package.version;
 
-          src = pkgs.lib.cleanSource ./.;
-          # FIX THIS HASH
-          cargoHash = "sha256-uCSmflKcxKTm62Gcp8mTIJctDivnp2qtO6chIck1BmU=";
-          useFetchCargoVendor = true;
-        };
+        src = pkgs.lib.cleanSource ./.;
+        # FIX THIS HASH
+        cargoHash = "sha256-uCSmflKcxKTm62Gcp8mTIJctDivnp2qtO6chIck1BmU=";
+        useFetchCargoVendor = true;
+      };
     in {
       default = rust-package;
     });

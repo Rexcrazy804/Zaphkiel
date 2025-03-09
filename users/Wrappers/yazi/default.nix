747966403b6ab9b567f3db7eb4eeea0709886082
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-09 14:54:46 +0530

    yazi: functionality to send files over kdeconnect

--- users/Wrappers/yazi/default.nix
+++ users/Wrappers/yazi/default.nix
@@ -1,14 +1,42 @@
-{pkgs, ...}:
-pkgs.symlinkJoin {
-  name = "yazi-wrapper";
-  paths = [
-    pkgs.yazi
-    pkgs.ripdrag
-  ];
-  buildInputs = [pkgs.makeWrapper];
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  kde-send =
+    pkgs.writers.writeNuBin "kde-send"
+    {
+      makeWrapperArgs = [
+        "--prefix"
+        "PATH"
+        ":"
+        "${lib.makeBinPath [pkgs.fzf]}"
+      ];
+    }
+    /*
+    nu
+    */
+    ''
+      def main [...files] {
+        let device = kdeconnect-cli -a --name-only | fzf
 
-  postBuild = ''
-    wrapProgram $out/bin/yazi \
-      --set-default YAZI_CONFIG_HOME ${./config}
-  '';
-}
+        for $file in $files {
+          kdeconnect-cli -n $"($device)" --share $"($file)"
+        }
+      }
+    '';
+in
+  pkgs.symlinkJoin {
+    name = "yazi-wrapper";
+    paths = [
+      kde-send
+      pkgs.yazi
+      pkgs.ripdrag
+    ];
+    buildInputs = [pkgs.makeWrapper];
+
+    postBuild = ''
+      wrapProgram $out/bin/yazi \
+        --set-default YAZI_CONFIG_HOME ${./config} \
+    '';
+  }

762966295633a65985d94e9b074c11f691580b4d
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-28 23:32:21 +0530

    Seraphine + graphics/intel: change drivers

--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -11,11 +11,18 @@
   };
 
   config = lib.mkIf config.graphicsModule.intel.enable {
+    # WARN too lazy to futher modularize this maybe re use nixos-hardware's module
     hardware.graphics.extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      # intel-media-driver # LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       libvdpau-va-gl
+      intel-ocl
     ];
+    # do I really need this?
+    # hardware.graphics.extraPackages32 = with pkgs; [
+    #   # intel-media-driver-32 # LIBVA_DRIVER_NAME=iHD
+    #   driversi686Linux.intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+    # ];
     environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
   };
 }

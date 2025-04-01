99d33b40f220bce95ac67ac9586fec22932ec19b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-04-01 19:24:02 +0530

    nvim: lua_ls conf tweaks to simplify luaCATS completions + cmdheight=1

--- users/Wrappers/nvim/init.lua
+++ users/Wrappers/nvim/init.lua
@@ -1,7 +1,7 @@
 -- vim options
 vim.o.number = true
 vim.o.termguicolors = true
-vim.o.cmdheight = 0
+-- vim.o.cmdheight = 0
 vim.o.relativenumber = true
 vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
@@ -301,6 +301,24 @@ require("lz.n").load {
         on_attach = on_attach,
         capabilities = capabilities,
         on_init = function(client)
+          local library = {}
+
+          -- don't waste time loading vim stuff if I am not in the nixos
+          -- configuration directory
+          if vim.fn.getcwd():find("nixos") then
+            library[#library+1] = vim.env.VIMRUNTIME
+          end
+
+          -- this is for getting LuaCats completions from devEnvs and is
+          -- entirely custom basically read the env var [ string of ':'
+          -- seperated paths] and tokenizes it using the gmatch function the
+          -- regex matches every continuos sequence of characters that is not
+          -- ':' and appends it to the library variable
+          local catsLibs = os.getenv("LUACATS_LIB") or ""
+          for lib in catsLibs:gmatch("([^:]+)") do
+            library[#library+1] = lib
+          end
+
           if client.workspace_folders then
             local path = client.workspace_folders[1].name
             if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
@@ -308,15 +326,10 @@ require("lz.n").load {
             end
           end
           client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-              version = 'LuaJIT'
-            },
+            runtime = { version = 'LuaJIT' },
             workspace = {
               checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                "./result/library"
-              }
+              library = library,
             }
           })
         end,

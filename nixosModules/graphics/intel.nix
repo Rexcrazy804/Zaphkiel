585e76fb8d91df30a0c4e7e00e6f26fb4b2b63c7
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-04-03 13:26:00 +0530

    Graphics/Intel: include media driver (req wayland) + vaapi hybrid

--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -13,16 +13,16 @@
   config = lib.mkIf config.graphicsModule.intel.enable {
     # WARN too lazy to futher modularize this maybe re use nixos-hardware's module
     hardware.graphics.extraPackages = with pkgs; [
-      # intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      # LIBVA_DRIVER_NAME=iHD
+      intel-media-driver 
+      # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      (intel-vaapi-driver.override {
+        enableHybridCodec = true;
+      }) 
       libvdpau-va-gl
       intel-ocl
     ];
-    # do I really need this?
-    # hardware.graphics.extraPackages32 = with pkgs; [
-    #   # intel-media-driver-32 # LIBVA_DRIVER_NAME=iHD
-    #   driversi686Linux.intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-    # ];
     environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
+    # environment.sessionVariables = {LIBVA_DRIVER_NAME = "i965";}; # Force intel-vaapi-driver
   };
 }

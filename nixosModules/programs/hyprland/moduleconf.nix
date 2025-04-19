d83b81c5c48d57d5d86c152952dfe4aca0514330
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-04-20 00:36:05 +0400

    Hyprland: seperate scripts into scripts.nix

--- nixosModules/programs/hyprland/moduleconf.nix
+++ nixosModules/programs/hyprland/moduleconf.nix
@@ -1,51 +1,5 @@
 pkgs: let
-  # small script to send files over kde connect on yazi
-  kde-send = pkgs.writers.writeNuBin "kde-send" ''
-    def main [...files] {
-      let device = kdeconnect-cli -a --name-only | fzf
-
-      for $file in $files {
-        kdeconnect-cli -n $"($device)" --share $"($file)"
-      }
-    }
-  '';
-  gpurecording = pkgs.writers.writeNuBin "gpurecording" ''
-    def "main start" [geometry?: bool = false] {
-      let recording_count = ps | where name =~ wl-screenrec | length
-      if $recording_count > 0 {
-        notify-send -t 2000 -i nix-snowflake-white "Failed to start Recording" "Recording in Progress"
-        return
-      }
-      notify-send -t 2000 -i nix-snowflake-white "Recording started" "Gpu screen recording has begun"
-      if $geometry {
-        wl-screenrec -g $"(slurp)" --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
-      } else {
-        wl-screenrec --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
-      }
-      notify-send -t 2000 -i nix-snowflake-white "Recording stopped" "Gpu screen recording has concluded"
-    }
-
-    def "main stop" [] {
-      try {
-        ps
-        | where name =~ wl-screenrec
-        | kill -s 2 $in.0.pid
-      } catch {
-        notify-send -t 2000 -i nix-snowflake-white "Failed to stop Recording" "No active recording to be stopped"
-      }
-    }
-
-    def main [] {
-      print "Available subcommands:"
-      print "start                 - Start gpu recording"
-      print "stop                  - Stop gpu recording"
-    }
-  '';
-  cowask = pkgs.writers.writeNuBin "cowask" ''
-    def main [question] {
-      cowsay $"($question)? \n (if (random bool) { 'yes lol' } else {'no fuck you' })"
-    }
-  '';
+  scripts = import ./scripts.nix {inherit pkgs;};
   flameshot = pkgs.flameshot.override {enableWlrSupport = true;};
 in {
   programs.hyprland = {
@@ -85,6 +39,7 @@ in {
     pkgs.fuzzel
     pkgs.wrappedPkgs.fzf
     pkgs.wl-screenrec
+    pkgs.libnotify
     flameshot
 
     # yazi + deps
@@ -92,9 +47,9 @@ in {
     pkgs.ripdrag
 
     # nushell scripts
-    kde-send
-    gpurecording
-    cowask
+    scripts.kde-send
+    scripts.gpurecording
+    scripts.cowask
   ];
 
   # required when kde plasma is not installed .w.
---
+++ nixosModules/programs/hyprland/scripts.nix
@@ -0,0 +1,49 @@
+{pkgs}: {
+  # small script to send files over kde connect on yazi
+  kde-send = pkgs.writers.writeNuBin "kde-send" ''
+    def main [...files] {
+      let device = kdeconnect-cli -a --name-only | fzf
+
+      for $file in $files {
+        kdeconnect-cli -n $"($device)" --share $"($file)"
+      }
+    }
+  '';
+  gpurecording = pkgs.writers.writeNuBin "gpurecording" ''
+    def "main start" [geometry?: bool = false] {
+      let recording_count = ps | where name =~ wl-screenrec | length
+      if $recording_count > 0 {
+        notify-send -t 2000 -i nix-snowflake-white "Failed to start Recording" "Recording in Progress"
+        return
+      }
+      notify-send -t 2000 -i nix-snowflake-white "Recording started" "Gpu screen recording has begun"
+      if $geometry {
+        wl-screenrec -g $"(slurp)" --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
+      } else {
+        wl-screenrec --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
+      }
+      notify-send -t 2000 -i nix-snowflake-white "Recording stopped" "Gpu screen recording has concluded"
+    }
+
+    def "main stop" [] {
+      try {
+        ps
+        | where name =~ wl-screenrec
+        | kill -s 2 $in.0.pid
+      } catch {
+        notify-send -t 2000 -i nix-snowflake-white "Failed to stop Recording" "No active recording to be stopped"
+      }
+    }
+
+    def main [] {
+      print "Available subcommands:"
+      print "start                 - Start gpu recording"
+      print "stop                  - Stop gpu recording"
+    }
+  '';
+  cowask = pkgs.writers.writeNuBin "cowask" ''
+    def main [question] {
+      cowsay $"($question)? \n (if (random bool) { 'yes lol' } else {'no fuck you' })"
+    }
+  '';
+}

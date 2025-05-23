14410da38b59509830a6a4c0f76733ab9089c4d2
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-05-24 00:22:31 +0400

    Pkgs: KokCursor to reproducable link, thanks caesus :>

--- pkgs/kokCursor.nix
+++ pkgs/kokCursor.nix
@@ -2,7 +2,14 @@
 stdenv.mkDerivation {
   name = "kokomiIcon";
   src = builtins.fetchTarball {
-    url = "https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1719068798/Kokomi.tar.gz?response-content-disposition=attachment%3B%2520Kokomi.tar.gz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20250519%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250519T165709Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=42cc7277477d7762ddb5956744984d2bbeae7d9a7513dde2a164d7cb6f9f2869";
+    url = let 
+      proto = "https";
+      domain = ["chibisafe" "crispy-caesus" "eu"];
+      file = "WUfAKD3Ct2y3";
+      filetype = "gz";
+    in 
+      proto + "://" + (builtins.concatStringsSep "." domain) + "/" + file + "." + filetype;
+
     sha256 = "0cb2v94n14i245p97m2r3r09p293wja5rypkhh87pzhnanam5sa4";
   };
 

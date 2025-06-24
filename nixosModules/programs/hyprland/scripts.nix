360661767ffc017f95be4ad807f27b9e5630864d
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-06-25 01:26:45 +0400

    Hyprland: rewrite gpurecording script in fish

--- nixosModules/programs/hyprland/scripts.nix
+++ nixosModules/programs/hyprland/scripts.nix
@@ -9,37 +9,51 @@
       }
     }
   '';
-  gpurecording = pkgs.writers.writeNuBin "gpurecording" ''
-    def "main start" [geometry?: string = ""] {
-      let recording_count = ps | where name =~ wl-screenrec | length
-      if $recording_count > 0 {
-        notify-send -t 2000 -i nix-snowflake-white "Failed to start Recording" "Recording in Progress"
+  gpurecording = pkgs.writers.writeFishBin "gpurecording" ''
+    function display_help
+      echo "Usage: gpurecording <options>"
+      echo ""
+      echo "Options:"
+      echo "--help              - display this help"
+      echo "--start [geometry]  - start gpu recording"
+      echo "--stop              - stop gpu recording"
+    end
+
+    function start_recording -a geometry
+      if test -n "$(pidof wl-screenrec)"
+        notify-send "Failed to start Recording" "Recording in Progress"
         return
-      }
-      notify-send -t 2000 -i nix-snowflake-white "Recording started" "Gpu screen recording has begun"
-      if ($geometry | is-empty) {
-        wl-screenrec --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
-      } else {
-        wl-screenrec $"-g($geometry | str trim)" --audio --audio-device "alsa_output.pci-0000_00_1f.3.analog-stereo.monitor" -f $"($env.HOME)/Videos/recording-(^date +%d%h%m_%H%M%S).mp4"
-      }
-      notify-send -t 2000 -i nix-snowflake-white "Recording stopped" "Gpu screen recording has concluded"
-    }
+      end
 
-    def "main stop" [] {
-      try {
-        ps
-        | where name =~ wl-screenrec
-        | kill -s 2 $in.0.pid
-      } catch {
-        notify-send -t 2000 -i nix-snowflake-white "Failed to stop Recording" "No active recording to be stopped"
-      }
-    }
+      set recording_file $HOME/Videos/recording-$(date +%d%h%m_%H%M%S)
+      notify-send -t 2000  "Recording started" "Gpu screen recording has begun"
+      wl-screenrec $(if test -n "$geometry"; echo -n -g$geometry; end) \
+        --codec hevc \
+        --audio \
+        --audio-device alsa_output.pci-0000_00_1f.3.analog-stereo.monitor \
+        -f $recording_file.mkv
 
-    def main [] {
-      print "Available subcommands:"
-      print "start                 - Start gpu recording"
-      print "stop                  - Stop gpu recording"
-    }
+      notify-send -t 2000  "Recording stopped" "Gpu screen recording has concluded"
+
+      # left in here in case i forgor how to compress shit
+      # ffmpeg -i $recording_file.mkv -vcodec libx265 -crf 28 $recording_file.mp4
+    end
+
+    function stop_recording
+      set wlpid "$(pidof wl-screenrec)"
+      if test -z "$wlpid"
+        notify-send "Failed to stop Recording" "No active recording in progress"
+      else
+        kill -s 2 $wlpid
+      end
+    end
+
+    switch $argv[1]
+      case --help; display_help;
+      case start; start_recording $argv[2];
+      case stop; stop_recording;
+      case '*'; echo "invalid option: $argv[1]" & display_help;
+    end
   '';
   cowask = pkgs.writers.writeFishBin "cowask" ''
     set message $argv[1]

644af784a247b32a497509efcbee9283487582e9
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-06-25 01:47:17 +0400

    Sddm: customize wallpaper

--- nixosModules/programs/sddm.nix
+++ nixosModules/programs/sddm.nix
@@ -17,14 +17,39 @@
 
   config = let
     cfg = config.progModule.sddm-custom-theme;
-    sddm-theme = pkgs.sddm-silent.override {
-      theme = "rei";
-      theme-overrides = {
-        "LoginScreen.LoginArea.Avatar" = {
-          border-radius = 10;
+    sddm-theme = let
+      zero-bg = pkgs.fetchurl {
+        url = "https://www.desktophut.com/files/kV1sBGwNvy-Wallpaperghgh2Prob4.mp4";
+        hash = "sha256-VkOAkmFrK9L00+CeYR7BKyij/R1b/WhWuYf0nWjsIkM=";
+      };
+
+      zero-thumb = pkgs.runCommandWith {
+        name = "thumbnail.png";
+        derivationArgs.nativeBuildInputs = [pkgs.ffmpeg];
+      } ''
+        ffmpeg -i ${zero-bg} -vf "select=eq(n\,34)" -vframes 1 $out
+      '';
+    in
+      pkgs.sddm-silent.override {
+        theme = "rei";
+        extraBackgrounds = [zero-bg zero-thumb];
+        theme-overrides = {
+          "LoginScreen.LoginArea.Avatar" = {
+            shape = "circle";
+            active-border-size = 0;
+            inactive-border-size = 0;
+          };
+          "LoginScreen" = {
+            background = "${zero-bg.name}";
+          };
+          "LockScreen" = {
+            background = "${zero-bg.name}";
+          };
+          "General" = {
+            animated-background-placeholder = "${zero-thumb.name}";
+          };
         };
       };
-    };
   in
     lib.mkIf cfg.enable {
       # changes might require a restart to be reflected correctly without errors

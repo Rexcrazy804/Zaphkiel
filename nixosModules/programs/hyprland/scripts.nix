032965c3f669653652e43e678ab8b7b4156c0d8a
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-06-26 12:33:05 +0400

    Hyprland: recorder saves to mp4 now

--- nixosModules/programs/hyprland/scripts.nix
+++ nixosModules/programs/hyprland/scripts.nix
@@ -31,7 +31,7 @@
         --codec hevc \
         --audio \
         --audio-device alsa_output.pci-0000_00_1f.3.analog-stereo.monitor \
-        -f $recording_file.mkv
+        -f $recording_file.mp4
 
       notify-send -t 2000  "Recording stopped" "Gpu screen recording has concluded"
 

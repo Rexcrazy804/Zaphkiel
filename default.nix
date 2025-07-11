ad73b1554a4d984afd379c395c679877748ee6bf
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-07-11 22:56:33 +0400

    Default: we do a lil bit of DRY

--- default.nix
+++ default.nix
@@ -1,58 +1,58 @@
 # you may leverage the following command to build systems using this
 # sudo nixos-rebuild --no-reexec --file ./default.nix -A <hostName> <boot|test|switch|...>
+# why am I mentioning this? well, others before my didn't an I spent weeks
+# tryna fucking do this. Thanks to Quinz for all the npins repos he shared
 let
-  inherit (builtins) mapAttrs;
-  sources = import ./npins;
-  pkgs = import sources.nixpkgs {};
-  lazysources = mapAttrs (k: v: v {inherit pkgs;}) sources;
-  nixosConfig = import (sources.nixpkgs + "/nixos/lib/eval-config.nix");
-  overlays = builtins.attrValues {
-    internal = import ./pkgs/overlays/internal.nix {inherit sources;};
+  inherit (builtins) mapAttrs attrValues;
+  # https://github.com/andir/npins?tab=readme-ov-file#using-the-nixpkgs-fetchers
+  src = import ./npins;
+  pkgs = import src.nixpkgs {};
+  sources = mapAttrs (k: v: v {inherit pkgs;}) src;
+
+  overlays = attrValues {
+    internal = import ./pkgs/overlays/internal.nix {sources = src;};
     lix = import ./pkgs/overlays/lix.nix {lix = null;};
-    # temporary
-    npins = import ./pkgs/overlays/npins.nix;
+    npins = import ./pkgs/overlays/npins.nix; # temporary
   };
-  # modulesPath = sources.nixpkgs + "/nixos/modules";
+
+  # here to remind me of the meaningless of the universe
+  # and why I should play Nier:Automata again
+  # modulesPath = src.nixpkgs + "/nixos/modules";
   # baseModules = builtins.map (path: modulesPath + "/" + path) (import ./baseModules.nix);
+
+  nixosConfig = {
+    hostName,
+    extraUsers ? [],
+    extraModules ? [],
+  }:
+    import (src.nixpkgs + "/nixos/lib/eval-config.nix") {
+      system = null;
+      specialArgs = {
+        inherit sources;
+        users = ["rexies"] ++ extraUsers;
+      };
+      modules =
+        [
+          {nixpkgs.overlays = overlays;}
+          ./hosts/${hostName}/configuration.nix
+          ./users
+        ]
+        ++ extraModules;
+    };
 in {
   Persephone = nixosConfig {
-    system = null;
-    specialArgs = {
-      sources = lazysources;
-      users = ["rexies"];
-    };
-
-    modules = [
-      {nixpkgs.overlays = overlays;}
-      ./hosts/Persephone/configuration.nix
-      ./nixosModules
-      ./users
-    ];
+    hostName = "Persephone";
+    extraModules = [./nixosModules];
   };
 
   Seraphine = nixosConfig {
-    specialArgs = {
-      sources = lazysources;
-      users = ["rexies"];
-    };
-    modules = [
-      {nixpkgs.overlays = overlays;}
-      ./hosts/Seraphine/configuration.nix
-      ./nixosModules
-      ./users
-    ];
+    hostName = "Seraphine";
+    extraModules = [./nixosModules];
   };
 
   Aphrodite = nixosConfig {
-    specialArgs = {
-      sources = lazysources;
-      users = ["rexies" "sivanis"];
-    };
-    modules = [
-      {nixpkgs.overlays = overlays;}
-      ./hosts/Aphrodite/configuration.nix
-      ./users
-      ./nixosModules/server-default.nix
-    ];
+    hostName = "Aphrodite";
+    extraUsers = ["sivanis"];
+    extraModules = [./nixosModules/server-default.nix];
   };
 }

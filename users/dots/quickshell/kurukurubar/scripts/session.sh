c45cee3c1c8432eed831b9857e50dda5ab74ab66
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-07-24 02:03:42 +0400

    add comment for non nix stuff, fix later

--- users/dots/quickshell/kurukurubar/scripts/session.sh
+++ users/dots/quickshell/kurukurubar/scripts/session.sh
@@ -7,6 +7,8 @@ ENTRIES=()
 
 # jesus christ I really hate this shity bash crap
 for SES in ${SESSIONS[@]}; do
+  # TODO for add each entry to the ENTRIES rather than just the HEAD
+  # this currently works for nix but for non nix this may not be the case
   ENTRIES+=($(find $SES -name "*.desktop" | head -1))
 done
 

4b116698937380d4cca8f02a339b1cca0eed3578
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-07-27 03:04:10 +0400

    default.nix: easy interface to consume stuff the non flake way :D

---
+++ default.nix
@@ -0,0 +1,56 @@
+# TODO: use v6 format once that hits nixpkgs-unstable
+{
+  sources' ? import ./npins,
+  pkgs ? import sources'.nixpkgs {},
+  quickshell ? import (sources'.quickshell) {},
+  mnw ? sources'.mnw,
+}: let
+  inherit (pkgs.lib) fix;
+  # I wanna use a top level let in to remove the import ./npins redundancy but
+  # that doesn't look cute so here we are .w.
+  sources = (import ./npins) // sources';
+in
+  fix (self: {
+    packages = {
+      mpv-wrapped = pkgs.callPackage ./pkgs/mpv {};
+      librebarcode = pkgs.callPackage ./pkgs/librebarcode.nix {};
+      kokCursor = pkgs.callPackage ./pkgs/kokCursor.nix {};
+
+      # speening the kuru kuru
+      kurukurubar-unstable = pkgs.callPackage ./pkgs/kurukurubar.nix {
+        inherit quickshell;
+        inherit (self.packages.scripts) gpurecording;
+      };
+      kurukurubar = self.packages.kurukurubar-unstable.override {
+        inherit (pkgs) quickshell;
+        configPath = (sources.zaphkiel) + "/users/dots/quickshell/kurukurubar";
+      };
+
+      nixvim-minimal = import ./pkgs/nvim.nix {
+        inherit pkgs mnw;
+      };
+      nixvim = self.packages.nixvim-minimal.override (prev: {
+        extraBinPath =
+          prev.extraBinPath
+          ++ [
+            # language servers
+            pkgs.nil
+            pkgs.lua-language-server
+            pkgs.kdePackages.qtdeclarative
+            # formatter
+            pkgs.alejandra
+          ];
+      });
+
+      booru-images = let
+        imgBuilder = pkgs.callPackage (sources.booru-flake + "/nix/imgBuilder.nix");
+      in (pkgs.lib.attrsets.mergeAttrsList (
+        builtins.map (x: {${"i" + x.id} = imgBuilder x;}) (import ./nixosModules/programs/booru-flake/imgList.nix)
+      ));
+
+      # some cute scripts
+      scripts = import ./pkgs/scripts {inherit (pkgs) lib callPackage;};
+    };
+
+    nixosModules.kurukuruDM = ./nixosModules/exported/kurukuruDM.nix;
+  })

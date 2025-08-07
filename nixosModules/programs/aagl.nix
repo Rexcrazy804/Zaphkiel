c2cd688b41126deb3210ba7bb797119cf1f0bd7c
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-07 16:27:19 +0400

    Summary: update aagl to fork + new format npins: switch to my fork of aagl (temp) nixosModules: apply overlay explicitly

--- nixosModules/programs/aagl.nix
+++ nixosModules/programs/aagl.nix
@@ -7,11 +7,15 @@
   imports = [(sources.aagl + "/module")];
   options.zaphkiel.programs.anime-games.enable = lib.mkEnableOption "anime games";
   config = lib.mkIf config.zaphkiel.programs.anime-games.enable {
+    nixpkgs.overlays = [
+      (import sources.rust-overlay)
+      (import (sources.aagl + "/overlay.nix"))
+    ];
     nix.settings.extra-substituters = ["https://ezkea.cachix.org"];
     nix.settings.extra-trusted-public-keys = ["ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="];
     programs = {
       anime-game-launcher.enable = lib.mkDefault false;
-      honkers-railway-launcher.enable = lib.mkDefault true;
+      honkers-railway-launcher.enable = lib.mkDefault false;
       sleepy-launcher.enable = lib.mkDefault true;
     };
   };

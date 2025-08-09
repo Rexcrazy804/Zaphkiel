2fe13a14126ef41021d91a50a3a3b34ec91de7c3
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-09 13:24:07 +0400

    Summary: qs update npins: switch qs pin to my fork's overridable-qs-unwrapped branch pkgs: exported overlay now patches qs on null to support greetd fingerprint

--- pkgs/overlays/exported.nix
+++ pkgs/overlays/exported.nix
@@ -25,17 +25,27 @@ in {
   quickshell =
     if (quickshell == null)
     then
-      callPackage (sources.quickshell) {
-        gitRev = sources.quickshell.revision;
-        withJemalloc = true;
-        withQtSvg = true;
-        withWayland = true;
-        withX11 = false;
-        withPipewire = true;
-        withPam = true;
-        withHyprland = true;
-        withI3 = false;
-      }
+      (callPackage (sources.quickshell + "/wrapper.nix") {
+        quickshell-unwrapped =
+          (callPackage (sources.quickshell) {
+            gitRev = sources.quickshell.revision;
+            withJemalloc = true;
+            withQtSvg = true;
+            withWayland = true;
+            withX11 = false;
+            withPipewire = true;
+            withPam = true;
+            withHyprland = true;
+            withI3 = false;
+          }).overrideAttrs (old: {
+            postPatch = ''
+              # required for finger print support
+              # temp measure till foxxed fixes it
+              substituteInPlace src/services/greetd/connection.cpp \
+                  --replace-fail "	if (!this->mResponseRequired) {" "	if (false) {"
+            '';
+          });
+      })
     else quickshell;
 
   mpv-wrapped = callPackage ../mpv {};

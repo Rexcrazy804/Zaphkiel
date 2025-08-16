8863b3e8858afb3a7b2fdc9e7ab595023950a73b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-16 15:31:30 +0400

    Pkgs: populate internal overlay

--- pkgs/overlays/internal.nix
+++ pkgs/overlays/internal.nix
@@ -1,6 +1,32 @@
-final: prev: {
+# kinda duplicated but fuck it, cleaner this way
+final: prev: let
+  inherit (final) callPackage;
+in {
+  # purely internal
   discord = prev.vesktop;
-  gnomon = final.callPackage ../gnomon.nix {};
+  gnomon = callPackage ../gnomon.nix {};
+
+  # kuru
+  quickshell = callPackage ../quickshell.nix {quickshell = null;};
+  kurukurubar-unstable = callPackage ../kurukurubar.nix {};
+  kurukurubar = (final.kurukurubar-unstable).override {inherit (prev) quickshell;};
+
+  # trivial
+  mpv-wrapped = callPackage ../mpv {};
+  librebarcode = callPackage ../librebarcode.nix {};
+  kokCursor = callPackage ../kokCursor.nix {};
+  npins = callPackage ../npins.nix {};
+  stash = callPackage (final.sources.stash + "/nix/package.nix") {};
+
+  # package sets
+  lanzaboote = callPackage ../lanzaboote {};
+  scripts = callPackage ../scripts {};
+  anime-launchers = callPackage ../anime-launchers {};
+  xvim = callPackage ../nvim {};
+
+  # lib
+  kokoLib = callPackage ../kokoLib {};
+  craneLib = callPackage (final.sources.crane + "/lib") {};
 
   # extra stuff to refer to later
 

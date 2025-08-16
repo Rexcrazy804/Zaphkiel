b1b89d58c7672d0d64e9a695103b3a3f0370f88e
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-16 13:06:18 +0400

    Default: clean up some nesting

--- default.nix
+++ default.nix
@@ -11,8 +11,9 @@
   pkgs ? import nixpkgs {config = {inherit allowUnfree;};},
   quickshell ? null,
   useNpinsV6 ? true,
-}: let
-  inherit (pkgs.lib) fix mapAttrs attrValues makeScope;
+}:
+pkgs.lib.fix (self: let
+  inherit (pkgs.lib) mapAttrs attrValues makeScope;
   inherit (pkgs) newScope mkShellNoCC;
 
   # WARNING
@@ -25,99 +26,98 @@
 
   # check this out if you wanna see everything exported
   exportedPackages = import ./pkgs;
-in
-  fix (self: {
-    overlays = {
-      lix = import ./pkgs/overlays/lix.nix {lix = null;};
-      # ensures that we don't add the overlay twice (straight up stolen from lix)
-      kurukurubar = _final: prev:
-        if prev ? kurukurubar-overlay-present
-        then {kurukurubar-overlay-present = 2;}
-        else {
-          kurukurubar-overlay-present = 1;
-          inherit (self.packages) kurukurubar kurukurubar-unstable;
-        };
-    };
-
-    packages = makeScope newScope (exportedPackages {
-      inherit quickshell pkgs sources;
-    });
-
-    nixosModules = {
-      kurukuruDM = {
-        imports = [./nixosModules/exported/kurukuruDM.nix];
-        nixpkgs.overlays = [self.overlays.kurukurubar];
+in {
+  overlays = {
+    lix = import ./pkgs/overlays/lix.nix {lix = null;};
+    # ensures that we don't add the overlay twice (straight up stolen from lix)
+    kurukurubar = _final: prev:
+      if prev ? kurukurubar-overlay-present
+      then {kurukurubar-overlay-present = 2;}
+      else {
+        kurukurubar-overlay-present = 1;
+        inherit (self.packages) kurukurubar kurukurubar-unstable;
       };
-      lanzaboote = {
-        imports = [(sources.lanzaboote + "/nix/modules/lanzaboote.nix")];
-        boot.lanzaboote.package = self.packages.lanzaboote.tool;
-      };
-    };
+  };
 
-    devShells.default = let
-      precommit = pkgs.writeShellScript "pre-commit" ''
-        if make chk FILES_STAGED=1; then
-          exit 0
-        else
-          make fmt FILES_STAGED=1
-          exit 1
+  packages = makeScope newScope (exportedPackages {
+    inherit quickshell pkgs sources;
+  });
+
+  nixosModules = {
+    kurukuruDM = {
+      imports = [./nixosModules/exported/kurukuruDM.nix];
+      nixpkgs.overlays = [self.overlays.kurukurubar];
+    };
+    lanzaboote = {
+      imports = [(sources.lanzaboote + "/nix/modules/lanzaboote.nix")];
+      boot.lanzaboote.package = self.packages.lanzaboote.tool;
+    };
+  };
+
+  devShells.default = let
+    precommit = pkgs.writeShellScript "pre-commit" ''
+      if make chk FILES_STAGED=1; then
+        exit 0
+      else
+        make fmt FILES_STAGED=1
+        exit 1
+      fi
+    '';
+  in
+    mkShellNoCC {
+      shellHook = ''
+        HOOKS=$(pwd)/.git/hooks
+        if ! [ -f "$HOOKS/pre-commit" ]; then
+          install ${precommit} $HOOKS/pre-commit
+          echo "[SHELL] created precommit hook :>"
+        elif ! cmp --silent $HOOKS/pre-commit ${precommit}; then
+          install ${precommit} $HOOKS/pre-commit
+          echo "[SHELL] updated precommit hook ^OwO^"
         fi
       '';
-    in
-      mkShellNoCC {
-        shellHook = ''
-          HOOKS=$(pwd)/.git/hooks
-          if ! [ -f "$HOOKS/pre-commit" ]; then
-            install ${precommit} $HOOKS/pre-commit
-            echo "[SHELL] created precommit hook :>"
-          elif ! cmp --silent $HOOKS/pre-commit ${precommit}; then
-            install ${precommit} $HOOKS/pre-commit
-            echo "[SHELL] updated precommit hook ^OwO^"
-          fi
-        '';
-        packages = attrValues {
-          # formatters
-          inherit (pkgs) alejandra luaformatter mdformat;
-          inherit (pkgs.qt6) qtdeclarative;
-          # yes I had to fucking write this
-          inherit (self.packages.scripts) qmlcheck;
-          # yes I hadda fix this
-          inherit (self.packages) mbake;
-          # make the cutest
-          inherit (pkgs) gnumake;
-        };
+      packages = attrValues {
+        # formatters
+        inherit (pkgs) alejandra luaformatter mdformat;
+        inherit (pkgs.qt6) qtdeclarative;
+        # yes I had to fucking write this
+        inherit (self.packages.scripts) qmlcheck;
+        # yes I hadda fix this
+        inherit (self.packages) mbake;
+        # make the cutest
+        inherit (pkgs) gnumake;
       };
-
-    nixosConfigurations = let
-      nixosSystem = import (nixpkgs + "/nixos/lib/eval-config.nix");
-      overlays = attrValues {
-        inherit (self.overlays) lix;
-        internal = import ./pkgs/overlays/internal.nix;
-        # for the people of the future,
-        # if you don't understand why this was done,
-        # just know I am a eval time racer
-        # this saves 1.1 seconds of eval time
-        internal' = final: prev:
-          (exportedPackages {
-            inherit sources;
-            pkgs = final;
-          })
-          final;
-      };
-      nixosHost = hostName:
-        nixosSystem {
-          system = null;
-          specialArgs = {inherit sources;};
-          modules = [
-            {nixpkgs.overlays = overlays;}
-            ./hosts/${hostName}/configuration.nix
-            ./users/rexies.nix
-            ./nixosModules
-          ];
-        };
-    in {
-      Persephone = nixosHost "Persephone";
-      Seraphine = nixosHost "Seraphine";
-      Aphrodite = nixosHost "Aphrodite";
     };
-  })
+
+  nixosConfigurations = let
+    nixosSystem = import (nixpkgs + "/nixos/lib/eval-config.nix");
+    overlays = attrValues {
+      inherit (self.overlays) lix;
+      internal = import ./pkgs/overlays/internal.nix;
+      # for the people of the future,
+      # if you don't understand why this was done,
+      # just know I am a eval time racer
+      # this saves 1.1 seconds of eval time
+      internal' = final: prev:
+        (exportedPackages {
+          inherit sources;
+          pkgs = final;
+        })
+        final;
+    };
+    nixosHost = hostName:
+      nixosSystem {
+        system = null;
+        specialArgs = {inherit sources;};
+        modules = [
+          {nixpkgs.overlays = overlays;}
+          ./hosts/${hostName}/configuration.nix
+          ./users/rexies.nix
+          ./nixosModules
+        ];
+      };
+  in {
+    Persephone = nixosHost "Persephone";
+    Seraphine = nixosHost "Seraphine";
+    Aphrodite = nixosHost "Aphrodite";
+  };
+})

c5b63fc9a5a8ff5e033e27e33d7ead29456fff71
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-17 13:25:48 +0400

    NixosModules: alias zaphkiel.secrets -> age.secrets

--- nixosModules/programs/age.nix
+++ nixosModules/programs/age.nix
@@ -1,10 +1,14 @@
 {
+  lib,
   sources,
   config,
   pkgs,
   ...
 }: {
-  imports = [(sources.agenix + "/modules/age.nix")];
+  imports = [
+    (sources.agenix + "/modules/age.nix")
+    (lib.mkAliasOptionModule ["zaphkiel" "secrets"] ["age" "secrets"])
+  ];
   environment.systemPackages = [(pkgs.callPackage "${sources.agenix}/pkgs/agenix.nix" {})];
   age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"] ++ builtins.map (username: "/home/${username}/.ssh/id_ed25519") config.zaphkiel.data.users;
 }

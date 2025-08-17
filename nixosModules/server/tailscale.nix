8106a5c591e1e06143a158b8e04d8f38c2494e97
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-17 15:00:06 +0400

    NixosModules: Tailscale correct optimization

--- nixosModules/server/tailscale.nix
+++ nixosModules/server/tailscale.nix
@@ -6,6 +6,11 @@
 }: let
   inherit (lib) mkEnableOption mkOption types mkIf optional mkForce getExe;
   cfg = config.zaphkiel.services.tailscale;
+  # adapted from https://github.com/piyoki/nixos-config/blob/master/system/networking/udp-gro-forwarding.nix
+  udp-grp-script = pkgs.writeShellScript "udp-gro-forwarding" ''
+    set -eux
+    ${getExe pkgs.ethtool} -K ${cfg.exitNode.networkDevice} rx-udp-gro-forwarding on rx-gro-list off;
+  '';
 in {
   options.zaphkiel.services.tailscale = {
     enable = mkEnableOption "Enable Tailscale Service";
@@ -37,17 +42,16 @@ in {
     # services.resolved.enable = true;
 
     # optimization for tailscale exitnode
-    services = {
-      networkd-dispatcher = mkIf cfg.exitNode.enable {
-        enable = true;
-        rules."50-tailscale" = {
-          onState = ["routable"];
-          script = ''
-            ${getExe pkgs.ethtool} -K ${cfg.exitNode.networkDevice} rx-udp-gro-forwarding on rx-gro-list off
-          '';
-        };
+    systemd.services."udp-gro-forwarding" = mkIf (cfg.exitNode.enable) {
+      before = ["tailscaled.service"];
+      wantedBy = ["multi-user.target"];
+      description = "UDP Gro Forwarding Service";
+      serviceConfig = {
+        ExecStart = "${udp-grp-script}";
+        Type = "oneshot";
       };
     };
+    systemd.services."tailscaled.service".wants = mkIf (cfg.exitNode.enable) ["udp-gro-forwarding.service"];
 
     # don't wait for this stupid thing to be done executing
     # i.e. when no wifi, system doesn't hang 3 minutes for this crap

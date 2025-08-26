97ac4424734d8555da0cc1d1052da1fd40968d63
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-26 16:05:50 +0400

    Revert "nixosModules/utils: adjust btrfs snapshot module"
    This reverts commit f4689516fe6ab2f18f7289a32eb56ea51dd0a431.

--- nixosModules/utils/btrfs-snapshot.nix
+++ nixosModules/utils/btrfs-snapshot.nix
@@ -6,15 +6,18 @@
   ...
 }: let
   inherit (lib) mkOption mkIf literalExpression mkMerge;
-  inherit (lib) map genAttrs pipe flatten;
-  inherit (lib.attrsets) attrValues attrNames filterAttrs mapAttrs;
+  inherit (lib) map genAttrs pipe filter;
   inherit (lib.strings) replaceString;
-  inherit (lib.types) listOf submodule str strMatching attrsOf;
+  inherit (lib.types) listOf submodule str strMatching enum;
 
   cfg = config.zaphkiel.utils.btrfs-snapshots;
 
   snapshotAttr = submodule {
     options = {
+      user = mkOption {
+        type = enum config.zaphkiel.data.users;
+        example = "myuser";
+      };
       subvolume = mkOption {
         type = str;
         example = "subvolume/path/from/home";
@@ -34,67 +37,53 @@
     };
   };
 
-  users = attrNames cfg;
+  users = map (x: x.user) cfg;
   tmpfilesFor = user: {
     rules = pipe cfg [
-      (filterAttrs (k: v: k == user))
-      attrValues
-      flatten
+      (filter (x: x.user == user))
       (map (x: "d '${config.users.users.${user}.home}/.snapshots/${x.subvolume}' - - - ${x.expiry} -"))
     ];
   };
 
-  timers = let
-    toTimer = user: list:
-      map (x: {
-        "snapshot-${replaceString "/" "-" x.subvolume}@${user}" = {
-          enable = true;
-          description = "Timer to start ${x.subvolume} backup for ${user}";
-          wantedBy = ["timers.target"];
-          timerConfig = {
-            OnCalendar = x.calendar;
-            Persistent = true;
-          };
+  timers =
+    map
+    (x: {
+      "snapshot-${replaceString "/" "-" x.subvolume}@${x.user}" = {
+        enable = true;
+        description = "Timer to start ${x.subvolume} backup for ${x.user}";
+        wantedBy = ["timers.target"];
+        timerConfig = {
+          OnCalendar = x.calendar;
+          Persistent = true;
         };
-      })
-      list;
-  in
-    pipe cfg [
-      (mapAttrs toTimer)
-      attrValues
-      flatten
-    ];
+      };
+    })
+    cfg;
 
-  services = let
-    script = subvol:
-      pkgs.writers.writeBashBin "backup" ''
-        set -euxo pipefail
-        ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot ~/${subvol} ~/.snapshots/${subvol}/$(date +%b%d)
-      '';
+  script = subvol:
+    pkgs.writers.writeBashBin "backup" ''
+      set -euxo pipefail
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot ~/${subvol} ~/.snapshots/${subvol}/$(date +%b%d)
+    '';
 
-    toService = user: list:
-      map (x: {
-        "snapshot-${replaceString "/" "-" x.subvolume}@${user}" = {
-          enable = true;
-          unitConfig.ConditionUser = user;
-          description = "Backup ${x.subvolume} for ${user}";
-          serviceConfig = {
-            Type = "oneshot";
-            ExecStart = "${script x.subvolume}/bin/backup";
-          };
+  services =
+    map
+    (x: {
+      "snapshot-${replaceString "/" "-" x.subvolume}@${x.user}" = {
+        enable = true;
+        unitConfig.ConditionUser = x.user;
+        description = "Backup ${x.subvolume} for ${x.user}";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${script x.subvolume}/bin/backup";
         };
-      })
-      list;
-  in
-    pipe cfg [
-      (mapAttrs toService)
-      attrValues
-      flatten
-    ];
+      };
+    })
+    cfg;
 in {
   options.zaphkiel.utils.btrfs-snapshots = mkOption {
-    type = attrsOf (listOf snapshotAttr);
-    default = {};
+    type = listOf snapshotAttr;
+    default = [];
     description = "a list describing files to back up";
     example = literalExpression ''
       snapshots = [
@@ -103,7 +92,7 @@ in {
     '';
   };
 
-  config = mkIf (cfg != {}) {
+  config = mkIf (cfg != []) {
     systemd.user.tmpfiles.users = genAttrs users tmpfilesFor;
     systemd.user.timers = mkMerge timers;
     systemd.user.services = mkMerge services;

7893d89e62d4da5a344683f2e716571454252f33
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-28 10:55:46 +0400

    hosts/persephone: re-nable lanzaboote :>

--- hosts/Persephone/configuration.nix
+++ hosts/Persephone/configuration.nix
@@ -24,7 +24,7 @@ in {
     };
 
     programs = {
-      lanzaboote.enable = false;
+      lanzaboote.enable = true;
       obs-studio.enable = false;
       steam.enable = false;
       hyprland.enable = true;

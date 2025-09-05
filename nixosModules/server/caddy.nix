cad6f2369d713dac3b18ea143cb6ae1147dec176
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-05 20:11:21 +0400

    Revert "nixosModules/caddy: update hash"
    This reverts commit 800e28b0618b17885348dd3bcccf2cdf3c475d7e.

--- nixosModules/server/caddy.nix
+++ nixosModules/server/caddy.nix
@@ -21,7 +21,7 @@ in {
       };
       hash = mkOption {
         type = singleLineStr;
-        default = "sha256-r68btTv8N7X/pKwGkP8FWg371rt+bZETXdEN0/ZlFJI=";
+        default = "sha256-0GsjeeJnfLsJywWzWwJcCDk5wjTSBwzqMBY7iHjPQa8=";
         description = "Hash for the plugin";
       };
     };

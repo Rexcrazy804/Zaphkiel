c3943781c48e967676a827ed40c145f9cf645bd3
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-09 19:09:46 +0400

    persephone: remove cowsay

--- hosts/Persephone/user-configuration.nix
+++ hosts/Persephone/user-configuration.nix
@@ -9,7 +9,7 @@
     inherit (pkgs.wineWowPackages) waylandFull;
     inherit (pkgs) legendary-heroic bottles winetricks mono umu-launcher;
     # terminal
-    inherit (pkgs) foot remmina cowsay;
+    inherit (pkgs) foot remmina;
     # from internal overlay
     inherit (pkgs) discord mpv-wrapped;
     inherit (pkgs.scripts) wallcrop legumulaunch;

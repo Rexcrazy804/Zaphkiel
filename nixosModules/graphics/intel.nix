4f324a61b5c3638c70d212e710bc3fa0e6565b9b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-10-07 23:07:44 +0400

    nixosModules/intel: refactored module

--- nixosModules/graphics/intel.nix
+++ nixosModules/graphics/intel.nix
@@ -5,19 +5,9 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf getExe mkOption;
-  inherit (lib.types) enum;
+  inherit (lib.types) enum listOf package;
+
   cfg = config.zaphkiel.graphics.intel;
-
-  intel-media-driver = [
-    pkgs.intel-media-driver
-    pkgs.libvdpau-va-gl
-    pkgs.vpl-gpu-rt
-  ];
-
-  runtimes = [
-    pkgs.intel-compute-runtime
-    pkgs.intel-ocl
-  ];
 in {
   options.zaphkiel.graphics.intel = {
     enable = mkEnableOption "intel graphics";
@@ -25,15 +15,31 @@ in {
       type = enum ["media-driver" "vaapi-driver"];
       default = "media-driver";
       description = "Hardware acceleration driver to use";
-      apply = x:
-        if x == "media-driver"
-        then intel-media-driver
+    };
+    driverPackages = mkOption {
+      readOnly = true;
+      default =
+        if cfg.hwAccelDriver == "media-driver"
+        then [
+          pkgs.intel-media-driver
+          pkgs.libvdpau-va-gl
+          pkgs.vpl-gpu-rt
+        ]
         else [pkgs.intel-vaapi-driver];
+      description = "readonly list of packages for selected driver";
+    };
+    runtimePackages = mkOption {
+      type = listOf package;
+      default = [
+        pkgs.intel-compute-runtime
+        pkgs.intel-ocl
+      ];
+      description = "list of runtime packages";
     };
   };
 
   config = mkIf (cfg.enable && config.zaphkiel.graphics.enable) {
-    hardware.graphics.extraPackages = runtimes ++ cfg.hwAccelDriver;
+    hardware.graphics.extraPackages = cfg.runtimePackages ++ cfg.driverPackages;
 
     # enables frame buffer compression
     boot.kernelParams = ["i915.enable_fbc=1"];
@@ -49,7 +55,7 @@ in {
     environment.sessionVariables = {
       # not supported yet
       # ANV_VIDEO_DECODE = 1;
-      LIBVA_DRIVER_NAME = mkIf (cfg.hwAccelDriver == intel-media-driver) "iHD";
+      LIBVA_DRIVER_NAME = mkIf (cfg.hwAccelDriver == "media-driver") "iHD";
     };
   };
 }

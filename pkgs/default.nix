514e50359843492c098d254cad8bd11cb4c5b391
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-10-25 03:04:00 +0400

    pkgs: some typo fixes and clean up

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,7 +14,7 @@ in {
   # for fingerprint to work in kurukuruDM,
   # you will need quickshell v0.2.1 minimum
   kurukurubar = callPackage ./kurukurubar.nix {inherit (self) librebarcode scripts;};
-  kurukurubar-unstable = lib.warn "kurukurubar-unstable depricated, use kurukurubar" self.kurukurubar;
+  kurukurubar-unstable = warn "kurukurubar-unstable depricated, use kurukurubar" self.kurukurubar;
 
   # trivial
   mpv-wrapped = callPackage ./mpv {};
@@ -23,20 +23,6 @@ in {
   stash = inputs.stash.packages.${system}.default;
   irminsul = callPackage ./irminsul {inherit (self.scripts) qmlcheck;};
   dust-racing = callPackage ./dust-racing.nix {};
-
-  # overriding the derrivation
-  # winslop = self.winboat.overrideAttrs (old: {
-  #   guestDrv = old.guestDrv.overrideAttrs (new': old': {
-  #     version = "0.8.6";
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "TibixDev";
-  #       repo = "winboat";
-  #       tag = "v${new'.version}";
-  #       hash = "sha256-Lvv+alPvQAcPFc8d+JC0hJX0sLjRL0/peJmMxiXNb0Q=";
-  #     };
-  #     vendorHash = "sha256-JglpTv1hkqxmcbD8xmG80Sukul5hzGyyANfe+GeKzQ4=";
-  #   });
-  # });
 
   # package sets
   scripts = callPackage ./scripts {};
@@ -47,6 +33,6 @@ in {
 
   # JUST SO YOU KNOW `nivxvim` WAS JUST WHAT I USED TO CALL MY nvim alright
   # I had ditched the nixvim project long long long ago but the name just stuck
-  nixvim-minimal = warn "Zahpkiel: `nixvim-minimal` depricated, please use `xvim.minimal` instead" self.xvim.minimal;
-  nixvim = warn "Zahpkiel: `nixvim` depricated please use `xvim.default` instead" self.xvim.default;
+  nixvim-minimal = warn "Zaphkiel: `nixvim-minimal` depricated, please use `xvim.minimal` instead" self.xvim.minimal;
+  nixvim = warn "Zaphkiel: `nixvim` depricated please use `xvim.default` instead" self.xvim.default;
 })

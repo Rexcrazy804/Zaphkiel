39faa614b100df04ae31d6678c4cbb4da776f5e4
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-10 22:59:56 +0400

    pkgs/mangowc: init

---
+++ pkgs/mangowc.nix
@@ -0,0 +1,68 @@
+# adapted from
+# https://github.com/DreamMaoMao/mangowc/blob/main/nix/default.nix
+{
+  sources,
+  lib,
+  libX11,
+  libinput,
+  libxcb,
+  libxkbcommon,
+  pcre2,
+  pixman,
+  pkg-config,
+  stdenv,
+  wayland,
+  wayland-protocols,
+  wayland-scanner,
+  xcbutilwm,
+  xwayland,
+  enableXWayland ? true,
+  meson,
+  ninja,
+  scenefx,
+  wlroots_0_19,
+  libGL,
+}:
+stdenv.mkDerivation {
+  pname = "mango";
+  version = "nightly";
+
+  src = sources.mangowc;
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    wayland-scanner
+  ];
+
+  buildInputs =
+    [
+      libinput
+      libxcb
+      libxkbcommon
+      pcre2
+      pixman
+      wayland
+      wayland-protocols
+      wlroots_0_19
+      scenefx
+      libGL
+    ]
+    ++ lib.optionals enableXWayland [
+      libX11
+      xcbutilwm
+      xwayland
+    ];
+
+  passthru.providedSessions = ["mango"];
+
+  meta = {
+    mainProgram = "mango";
+    description = "A streamlined but feature-rich Wayland compositor";
+    homepage = "https://github.com/DreamMaoMao/mango";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [];
+    platforms = lib.platforms.unix;
+  };
+}

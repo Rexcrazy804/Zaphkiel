8ad63badb847210a5aa02e9ddb883867ce271eb9
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-13 20:52:52 +0400

    dots/mangowc: improve uwsm activation

--- users/dots/mango/autostart.sh
+++ users/dots/mango/autostart.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-uwsm finalize
-# see if this is really needed
-# dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
+# append "wlroots" to XDG_CURRENT_DESKTOP if not already there
+case "A:${XDG_CURRENT_DESKTOP}:Z" in
+  *:wlroots:*) true ;;
+  *) export XDG_CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}:wlroots" ;;
+esac
+
+uwsm finalize XDG_CURRENT_DESKTOP XCURSOR_SIZE XCURSOR_THEME
+
+# services
 uwsm app -t service kurukurubar
 uwsm app -t service foot -- --server
 uwsm app -t service stash -- watch

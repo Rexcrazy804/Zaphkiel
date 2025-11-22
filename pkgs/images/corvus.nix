2090d126a9b2a1c93476052b9c2087923f5fe5d2
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-22 10:20:44 +0400

    pkgs/images: init

---
+++ pkgs/images/corvus.nix
@@ -0,0 +1,7 @@
+# homies, brume, the corvus sub dps support, out dmgs the ever living hell out of her
+# well, atleast till corvus fully ramps up :copium: T.T
+{fetchurl}:
+fetchurl {
+  url = "https://cdn.donmai.us/original/8c/5d/__rubuska_and_corvus_reverse_1999__8c5da40a6b3a247b20327f0c0d71d2b9.jpg";
+  hash = "sha256-Gzk5CRaMnu5WJUvg3SUpnS15FdrPvONcN5bBRdxIFtY=";
+}

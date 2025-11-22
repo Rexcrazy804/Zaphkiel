c4d42e6d44a9e91a5f504ef27831eac2fe6351cf
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-22 14:52:55 +0400

    modules/zaphkiel-data: addition of tailscale network info

--- modules/utils/zaphkiel-data.nix
+++ modules/utils/zaphkiel-data.nix
@@ -1,19 +1,39 @@
 {
-  dandelion.modules.zaphkiel-data = {lib, ...}: let
+  dandelion.modules.zaphkiel-data = {
+    lib,
+    config,
+    ...
+  }: let
     inherit (lib) mkOption;
     inherit (lib.types) listOf path str;
   in {
-    options = {
-      zaphkiel = {
-        data.users = mkOption {
-          type = listOf str;
-          default = [];
-          description = "list of users (duh)";
-        };
-        data.wallpaper = mkOption {
-          type = path;
-          description = "wallpaper path (duh)";
-        };
+    options.zaphkiel.data = {
+      users = mkOption {
+        type = listOf str;
+        default = [];
+        description = "list of users (duh)";
+      };
+      wallpaper = mkOption {
+        type = path;
+        description = "wallpaper path (duh)";
+      };
+      tailscale = mkOption {
+        readOnly = true;
+        default = lib.fix (self: {
+          Persephone = {
+            ipv4 = "100.110.70.18";
+            ipv6 = "fd7a:115c:a1e0::6a01:4614";
+          };
+          Aphrodite = {
+            ipv4 = "100.121.86.4";
+            ipv6 = "fd7a:115c:a1e0::6e01:5604";
+          };
+          Seraphine = {
+            ipv4 = "100.112.116.17";
+            ipv6 = "fd7a:115c:a1e0::eb01:7412";
+          };
+          self = self.${config.networking.hostName};
+        });
       };
     };
   };

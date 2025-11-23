b13209ced4525fa430df37e697425c8c74062f4b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-23 15:53:26 +0400

    modules/lib: addition of mkDotsModule

--- modules/lib.nix
+++ modules/lib.nix
@@ -4,10 +4,38 @@
   systems,
   ...
 }: let
-  inherit (nixpkgs.lib) getAttrs mapAttrs;
+  inherit (nixpkgs.lib) getAttrs mapAttrs isFunction;
 in {
   lib = {
     pkgsFor = getAttrs (import systems) nixpkgs.legacyPackages;
     eachSystem = fn: mapAttrs (system: pkgs: fn {inherit system pkgs;}) self.lib.pkgsFor;
+
+    # see modules/users/rexies.nix for usage
+    # TODO don't toHjem everything perhaps?
+    # dots => attrset like
+    #         { "pathInConfig" = "/pathInDotsFolder" or FN }
+    # FN => Function that returns a string
+    mkDotsModule = username: dots: {
+      config,
+      pkgs,
+      lib,
+      ...
+    }: let
+      inherit (config.hjem.users.${username}.impure) dotsDir;
+      toHjem = x: {source = x;};
+      args = {
+        inherit (self.packages.${pkgs.system}) sources;
+        inherit lib config;
+        dots = dotsDir;
+      };
+    in {
+      hjem.users.${username}.xdg.config.files = mapAttrs (_: dot:
+        toHjem (
+          if isFunction dot
+          then dot args
+          else (dotsDir + dot)
+        ))
+      dots;
+    };
   };
 }

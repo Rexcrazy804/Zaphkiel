13b5f1df05796ef23b04b6465fbdea3d969d3359
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-11-23 19:10:56 +0400

    pkgs/images: added kokomi

---
+++ pkgs/images/kokomi.nix
@@ -0,0 +1,16 @@
+{
+  fetchurl,
+  scripts,
+  runCommandNoCCLocal,
+}:
+runCommandNoCCLocal "kokomi.jpg" {
+  nativeBuildInputs = [scripts.wallcrop];
+  src = fetchurl {
+    url = "https://cdn.donmai.us/original/1a/2b/__sangonomiya_kokomi_genshin_impact_drawn_by_kiwiteamlp__1a2bd680aa1423cd74c769cf3f04f683.jpg";
+    hash = "sha256-swwgPlD0HjN9v+ukzd5Dqf46bftsJ5Srzf5ljjgPoHI=";
+  };
+  dontUnpack = true;
+}
+''
+  wallcrop $src 0 145 > $out
+''

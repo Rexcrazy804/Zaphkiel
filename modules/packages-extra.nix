960803d6b9324e4b3422a54ca6cd58c8d648a0d2
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-07 19:05:07 +0400

    modules/extra-pkgs: say no to stash wraping my wl-copy/paste >_>

--- modules/packages-extra.nix
+++ modules/packages-extra.nix
@@ -12,6 +12,16 @@
       inherit (self.packages.${system}) sources;
       mnw = mnw.lib;
     };
-    stash = stash.packages.${system}.default;
+    stash = let
+      stp = stash.packages.${system}.default;
+    in
+      pkgs.symlinkJoin {
+        inherit (stp) meta version pname;
+        paths = [stp];
+        postBuild = ''
+          rm $out/bin/wl-copy
+          rm $out/bin/wl-paste
+        '';
+      };
   });
 }

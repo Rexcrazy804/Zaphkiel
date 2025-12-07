8a9ea2349efe14d4c0d6b0905183ceda799670b1
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-07 17:50:00 +0400

    modules/fonts: fixed noto-emoji font name

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -8,7 +8,7 @@
       fontDir.enable = true;
       packages = lib.attrValues {
         inherit (pkgs.nerd-fonts) caskaydia-mono caskaydia-cove;
-        inherit (pkgs) noto-fonts noto-fonts-emoji noto-fonts-cjk-sans;
+        inherit (pkgs) noto-fonts noto-fonts-color-emoji noto-fonts-cjk-sans;
         inherit (pkgs) noto-fonts-cjk-serif material-symbols iosevka;
         inherit (self.packages.${pkgs.system}) librebarcode;
       };

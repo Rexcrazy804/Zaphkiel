b0fbcf01476a4f20c5f5c9f7d28a584bea559e0a
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-07 16:42:06 +0400

    modules/kuruDM-mango: fix breaking changes for mango 0.10.7

--- modules/programs/kuruDM-mango.nix
+++ modules/programs/kuruDM-mango.nix
@@ -8,17 +8,15 @@
     # use mangowc as base for kurukuruDM
     services.greetd.settings.default_session.command = let
       cfg = config.programs.kurukuruDM;
-    in let
-      mangoConfDir = pkgs.linkFarmFromDrvs "mango" [
-        (pkgs.writeShellScript "autostart.sh" ''
-          ${cfg.finalOpts} ${cfg.package}/bin/kurukurubar && pkill mango
-        '')
-        (pkgs.writeText "config.conf" ''
-          monitorrule=eDP-1,1,1,tile,0,1.25,0,0,1920,1080,60
-          cursor_theme=Kokomi_Cursor
-        '')
-      ];
+      autostart = pkgs.writeShellScript "autostart.sh" ''
+        ${cfg.finalOpts} ${cfg.package}/bin/kurukurubar && pkill mango
+      '';
+      mangoConf = pkgs.writeText "config.conf" ''
+        monitorrule=eDP-1,1,1,tile,0,1.25,0,0,1920,1080,60
+        cursor_theme=Kokomi_Cursor
+        exec-once = ${autostart}
+      '';
     in
-      lib.mkForce "env MANGOCONFIG=${mangoConfDir} ${self.packages.${pkgs.system}.mangowc}/bin/mango";
+      lib.mkForce "env ${self.packages.${pkgs.system}.mangowc}/bin/mango -c ${mangoConf}";
   };
 }

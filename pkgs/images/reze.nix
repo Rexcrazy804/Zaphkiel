662ac9cf4094f78942e38241f34b2afb7d1a4bad
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-15 22:17:41 +0400

    pkgs/images: added reze wall

---
+++ pkgs/images/reze.nix
@@ -0,0 +1,7 @@
+# Homies, after the movie, she be giving me the butterflies.
+# I'll treasure the memory of this woman.
+{fetchurl}:
+fetchurl {
+  url = "https://cdn.donmai.us/original/b8/a2/__reze_chainsaw_man_drawn_by_busuttt__b8a2fd187890a40b9d293dacbd6da2b0.jpg";
+  hash = "sha256-xF/1Rx/x4BLaj0mA8rWa67cq/+K6NdkOcCAB7R11+M0=";
+}

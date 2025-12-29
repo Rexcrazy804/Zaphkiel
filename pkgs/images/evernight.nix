eb252f76d7d333fda9e168f9862604301a1d37b7
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-12-29 13:18:06 +0400

    pkgs/images: added evernight

---
+++ pkgs/images/evernight.nix
@@ -0,0 +1,11 @@
+# pixiv:    https://www.pixiv.net/en/artworks/136398401
+# danbooru: https://danbooru.donmai.us/posts/10015220
+# this is EVERYTHING I love about ART
+# THIS IS SIMPLY STELLAR,a MASTERPIECE.
+# the composition, the perfectly contrasting the colors
+# simply etherial. bless qianqianjie for this piece
+{fetchurl}:
+fetchurl {
+  url = "https://cdn.donmai.us/original/1b/4e/__march_7th_evernight_and_evey_honkai_and_1_more_drawn_by_qianqianjie__1b4ecf8ed67f9b93b4b72848c37c782f.jpg";
+  hash = "sha256-bJ4Gqa6hOapl40PQMWZ1A4YPTcqvgiwjSYIrHEekY08=";
+}

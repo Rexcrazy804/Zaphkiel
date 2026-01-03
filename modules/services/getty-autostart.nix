de2229caf3de0374c307127df76accb53f6d44e4
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-01-03 20:56:55 +0400

    revert(fish): relocation of loginShellInit
    This reverts commit cf0def76ef827940c7e37af94cf5b33d381f4d97.

--- modules/services/getty-autostart.nix
+++ modules/services/getty-autostart.nix
@@ -1,8 +1,19 @@
 {
   dandelion.modules.getty-autostart = {
+    config,
+    lib,
+    ...
+  }: {
     services.getty = {
       autologinUser = "rexies";
       autologinOnce = true;
     };
+    programs.fish.loginShellInit =
+      lib.mkIf config.programs.uwsm.enable
+      ''
+        if uwsm check may-start;
+          exec uwsm start default
+        end
+      '';
   };
 }

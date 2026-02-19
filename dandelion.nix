96258dc73f860345dea4f7f994007022b53b3645
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-02-19 05:25:30 +0000

    dandelion: simplify warning message
    I have been using this for long enough now,
    that I can somewhat recommend this setup?
    The only issue I have faced so far merging functions.
    It is perfectly fine once you understand the limitation imposed by them,
    or AVOID DUPLICATED NAMESPACES entirely.
    
    Yours Lovingly,
    Rexiel Scarlet

--- dandelion.nix
+++ dandelion.nix
@@ -4,30 +4,17 @@
   # simply import ALL nix files in a directory
   recursiveImport = path: filter (hasSuffix ".nix") (filesystem.listFilesRecursive path);
 
-  # WARN I DO NOT ENCOURAGE THE USE OF THIS FUNCTION!!!!
+  # WARN
+  # Don't try to have duplicated dandelion.<namespace>.<entry>
+  # You have been warned.
   #
-  # be a normal person and use flake-parts or something similiar,
-  # you have been warned, follow this advice to keep your sanity.
-  #
-  # Use of this function will lead to error messages that don't
-  # inform you of **which** file the error occurs.
-  # Hence its down to you to figure that part out
-  #
-  # Now if you are insane like me,
-  # and want to NOT use flake-parts (or reinvent it)
-  # are comfortable diggin through trace to figure out what went wrong
-  # when things go wrong, feel free to use this
-  #
-  # Any recommendations / suggestions
-  # for improving this function are VERY WELCOME!
-  #
-  # NOTE an understanding of `recursiveUpdate`
-  # and the limitations it imposes is highly recommended
-  #
+  # Use the blow as a quick sanity check
   # $ cat modules/**/**.nix | grep "dandelion.modules.* = " | sort | uniq -d
-  # quick sanity check, don't try to have duplicated dandelion.<namespace>.<entry>
-  # well you CAN but if one is a funciton and the other isn't, welp won't work.
-  # I will cook up something later to do this within the funciton
+  #
+  # NOTE
+  # Now if you want to still use diplicated namespaces
+  # an understanding of `recursiveUpdate` is HIGHLY recomended.
+  # Just know that it can't merge functions.
   importModules = flip pipe [
     flatten
     (map (x:

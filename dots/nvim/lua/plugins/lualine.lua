2b35f6cae55f966fc8a7be1007539ecc7c74f4a7
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-06-19 00:10:27 +0400

    xvim: add jj integartion to lualine

--- dots/nvim/lua/plugins/lualine.lua
+++ dots/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,48 @@
+local M = {}
+
+M.change_id = nil
+M.desc = nil
+
+-- cute lil function to get the jujutsu status
+M.jj_status = function()
+  local cwd, err = vim.uv.cwd()
+  if err then return end
+  local sysobj = vim.system({
+    "jj",
+    "log",
+    "-r",
+    "@",
+    "--template",
+    "change_id.short(8) ++ description",
+    "--no-graph",
+  }, { cwd = cwd }, function() M.status = "err" end)
+
+  local result = sysobj:wait(100)
+  local status = vim.trim(result.stdout)
+  M.change_id = " " .. status:sub(1, 8)
+  M.desc = status:sub(9, status:len())
+end
+
+M.init = function()
+  vim.api.nvim_create_autocmd(
+    { "BufEnter" },
+    { pattern = "*", callback = function(_) M.jj_status() end }
+  )
+end
+
 return {
   "lualine.nvim",
   event = "VimEnter",
   after = function()
     if vim.g.vscode then return end
+    M.init()
     require("lualine").setup({
+      sections = {
+        lualine_b = { function() return M.change_id end, "diff", "diagnostics" },
+      },
       tabline = {
         lualine_a = { { "buffers", symbols = { alternate_file = "" } } },
+        lualine_c = { function() return M.desc end },
         lualine_x = {
           function()
             local status = require("direnv").statusline()

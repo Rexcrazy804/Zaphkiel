6e26ccbbb498d53e643f93027861c88b93e81390
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-06-19 05:43:10 +0000

    hosts/flora: added jj config

--- modules/hosts/flora.nix
+++ modules/hosts/flora.nix
@@ -73,6 +73,7 @@ in {
   dandelion.dots.nixos = mkDotsModule username {
     # terminal
     "git/config" = "/git/config";
+    "jj/config.toml" = "/jj/config.toml";
     "fish/themes" = {sources, ...}: sources.rosep-fish + "/themes";
     "fish/config.fish" = "/fish/config.fish";
     # NOTE: required bat cache --build before theme can be used

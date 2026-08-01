239b2304c5684ac71218293293576ebc527f2517
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-08-01 16:01:34 +0400

    modules/winboat: allow electron40 for winboat

--- modules/programs/winboat.nix
+++ modules/programs/winboat.nix
@@ -3,5 +3,10 @@
     virtualisation.docker.enable = true;
     environment.systemPackages = [pkgs.winboat];
     users.users.rexies.extraGroups = ["docker"];
+
+    # remove this once fixed upstream
+    nixpkgs.config.permittedInsecurePackages = [
+      "electron-40.10.5"
+    ];
   };
 }

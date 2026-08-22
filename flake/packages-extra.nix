0808d4dcf69790aa4af513240bf82e2fa875cd71
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-08-22 12:26:29 +0400

    pkgs/equibop: fix build droping override till next ver

--- flake/packages-extra.nix
+++ flake/packages-extra.nix
@@ -27,14 +27,15 @@
         '';
       };
 
-    equibop = (pkgs.equibop.overrideAttrs
-      (self: old: {
-        version = "3.2.2";
-        src = old.src.overrideAttrs (_: {hash = "sha256-foKgtyN1jr4+PHwJHTVXrYzWNVYtR1Sq8rLG4VEnujs=";});
-        node-modules = (
-          old.node-modules.overrideAttrs
-          (_: {outputHash = "sha256-TKFL47b+Xh8ChlSyXdhRY+zmPAnkHJss2vNblBvOSmw=";})
-        ).override {equibop = self;}; # this won't be required for overriding once https://github.com/NixOS/nixpkgs/pull/550390 is merged
-      })).override {electron_41 = pkgs.electron_43;};
+    equibop = pkgs.equibop;
+    # equibop = (pkgs.equibop.overrideAttrs
+    #   (self: old: {
+    #     version = "3.2.2";
+    #     src = old.src.overrideAttrs (_: {hash = "sha256-foKgtyN1jr4+PHwJHTVXrYzWNVYtR1Sq8rLG4VEnujs=";});
+    #     node-modules = (
+    #       old.node-modules.overrideAttrs
+    #       (_: {outputHash = "sha256-TKFL47b+Xh8ChlSyXdhRY+zmPAnkHJss2vNblBvOSmw=";})
+    #     ).override {equibop = self;}; # this won't be required for overriding once https://github.com/NixOS/nixpkgs/pull/550390 is merged
+    #   })).override {electron_41 = pkgs.electron_43;};
   });
 }

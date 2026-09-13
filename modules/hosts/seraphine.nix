a7b63b87331abda055e44b9731705c56b2b1de8c
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-09-13 12:32:50 +0400

    tree: rewrite part 11 / fix seraphine

--- modules/hosts/seraphine.nix
+++ modules/hosts/seraphine.nix
@@ -1,89 +1,89 @@
-{self, ...}: {
-  dandelion.hosts.Seraphine = {
-    pkgs,
-    config,
-    ...
-  }: let
-    pkgx = self.lib.mkPkgx' pkgs;
-  in {
-    imports = [
-      self.dandelion.users.rexies
-      self.dandelion.dots.rexies-cli
-      self.dandelion.dots.rexies-gui
-      self.dandelion.dots.rexies-mango
+{
+  config,
+  inputs,
+  ...
+}: let
+  inherit (config.nixpkgs.hostPlatform) system;
+  inherit (inputs.self.legacyPackages.${system}) images mpv-wrapped;
+  inherit (inputs.self) paths;
+in {
+  imports = [
+    ../users/rexies.nix
+    ../dots/rexies-cli.nix
+    ../dots/rexies-gui.nix
+    ../dots/rexies-mango.nix
 
-      self.dandelion.profiles.default
-      self.dandelion.profiles.mangowc
-      self.dandelion.profiles.workstation
+    ../profiles/default.nix
+    ../profiles/mangowc.nix
+    ../profiles/workstation.nix
 
-      self.dandelion.modules.intel
-      self.dandelion.modules.cups
-      self.dandelion.modules.caddy
-      self.dandelion.modules.jellyfin
-      self.dandelion.modules.getty-autostart
-    ];
+    ../hardware/intel.nix
+    ../services/cups.nix
+    ../services/caddy.nix
+    ../services/jellyfin.nix
+    ../services/getty-autostart.nix
+  ];
 
-    # info
-    system.stateVersion = "24.05";
-    networking.hostName = "Seraphine";
-    time.timeZone = "Asia/Dubai";
-    nixpkgs.hostPlatform = "x86_64-linux";
+  # info
+  system.stateVersion = "24.05";
+  networking.hostName = "Seraphine";
+  time.timeZone = "Asia/Dubai";
+  nixpkgs.hostPlatform = "x86_64-linux";
 
-    # zaphkiel opts
-    zaphkiel = {
-      data.wallpaper = pkgx.images.corvus;
-      secrets = {
-        tailAuth.file = self.paths.secrets + /secret5.age;
-        caddyEnv.file = self.paths.secrets + /secret10.age;
+  # zaphkiel opts
+  zaphkiel = {
+    data.wallpaper = images.corvus;
+    secrets = {
+      tailAuth.file = paths.secrets + /secret5.age;
+      caddyEnv.file = paths.secrets + /secret10.age;
+    };
+    programs = {
+      privoxy.forwards = [{domains = [".donmai.us" ".yande.re" "www.zerochan.net"];}];
+      shpool.users = ["rexies"];
+    };
+    graphics.intel.hwAccelDriver = "media-driver";
+    services = {
+      caddy = {
+        secretsFile = config.age.secrets.caddyEnv.path;
+        tsplugin.enable = true;
       };
-      programs = {
-        privoxy.forwards = [{domains = [".donmai.us" ".yande.re" "www.zerochan.net"];}];
-        shpool.users = ["rexies"];
-      };
-      graphics.intel.hwAccelDriver = "media-driver";
-      services = {
-        caddy = {
-          secretsFile = config.age.secrets.caddyEnv.path;
-          tsplugin.enable = true;
-        };
-        tailscale = {
-          operator = "rexies";
-          authFile = config.age.secrets.tailAuth.path;
-        };
+      tailscale = {
+        operator = "rexies";
+        authFile = config.age.secrets.tailAuth.path;
       };
     };
-
-    # user stuff
-    users.users."rexies".packages = [pkgx.mpv-wrapped];
-
-    # hardware
-    boot.kernelParams = ["i915.enable_guc=2"];
-    boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
-
-    # probably not required, but leaving it in for now
-    services.fstrim.enable = true;
-    # disabled autosuspend
-    services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
-    # temporarily setting it for Seraphine only
-    networking.networkmanager.wifi.backend = "iwd";
-    # Resolves wifi connectivity issues on Seraphine
-    boot.extraModprobeConfig = "options iwlwifi 11n_disable=1";
-
-    fileSystems = {
-      "/" = {
-        device = "/dev/disk/by-uuid/cd29f1f1-5adc-41dd-b13a-2ec03704fb9f";
-        fsType = "ext4";
-        options = ["discard"];
-      };
-      "/boot" = {
-        device = "/dev/disk/by-uuid/9972-2440";
-        fsType = "vfat";
-        options = ["fmask=0077" "dmask=0077"];
-      };
-    };
-
-    swapDevices = [
-      {device = "/dev/disk/by-uuid/abf01339-e9eb-4179-b5ef-91ef6f35af24";}
-    ];
   };
+
+  # user stuff
+  users.users."rexies".packages = [mpv-wrapped];
+
+  # hardware
+  boot.kernelParams = ["i915.enable_guc=2"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+
+  # probably not required, but leaving it in for now
+  services.fstrim.enable = true;
+  # disabled autosuspend
+  services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
+  # temporarily setting it for Seraphine only
+  networking.networkmanager.wifi.backend = "iwd";
+  # Resolves wifi connectivity issues on Seraphine
+  boot.extraModprobeConfig = "options iwlwifi 11n_disable=1";
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/cd29f1f1-5adc-41dd-b13a-2ec03704fb9f";
+      fsType = "ext4";
+      options = ["discard"];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/9972-2440";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/abf01339-e9eb-4179-b5ef-91ef6f35af24";}
+  ];
 }
